cce346d0378918dc9dce4849221248a5358e165b
WollyDev <wollywoll8844@gmail.com>
2025-09-19 14:39:37 +0200

    Update info.py

--- CustomBot/cogs/info.py
+++ CustomBot/cogs/info.py
@@ -7,7 +7,7 @@ class Info(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @slash_command()
+    @slash_command(description="Get info about the bot")
     async def info(self, ctx):
         embed = discord.Embed(
             title="Bot Information",
@@ -15,10 +15,34 @@ class Info(commands.Cog):
             color=discord.Color.green()
         )
 
-        embed.add_field(name="Version", value="0.3", inline=False)
-        embed.add_field(name="Developer", value="[WollyDev24](https://github.com/WollyDev24)", inline=False)
-        embed.add_field(name="Github", value="[Github Repo](https://github.com/WollyDev24/YouShadeBot/)", inline=False)
-        embed.set_footer(text="YouShadeBot is running in an Alpha State, be sure to check for Beta and Stable Updates!")
+        embed.add_field(
+            name="Version", 
+            value="V1.0.0", 
+            inline=True
+            )
+        embed.add_field(
+            name="Relase Type", 
+            value="Stable", 
+            inline=True
+            )
+        embed.add_field(
+            name="Developer", 
+            value="[WollyDev24](https://github.com/WollyDev24)", 
+            inline=False
+            )
+        embed.add_field(
+            name="Github", 
+            value="[Github Repo](https://github.com/WollyDev24/YouShadeBot/)", 
+            inline=False
+            )
+        embed.add_field(
+            name="Newest Update", 
+            value=" - Updadet Info Command\n - /changestatus is now /activity\n - Ping Command added\n - Greet channel is now in config.py\n - More options for /activity", 
+            inline=False
+            )
+        embed.set_footer(
+            text="You are running the Stable Version of YouShadeBot"
+            )
 
         await ctx.respond(embed=embed)
 
