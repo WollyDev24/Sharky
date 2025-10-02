29b5ea8ce08088fa8cca7b9c10e07bc6f41fe69c
WollyDev <wollywoll8844@gmail.com>
2025-10-02 19:18:15 +0200

    Update main.py

--- CustomBot/main.py
+++ CustomBot/main.py
@@ -8,6 +8,22 @@ status = discord.Status.online
 activity = discord.Activity(type=discord.ActivityType.watching, name="for hunry sharks")
 
 from config import server
+from config import owner
+from config import greet
+from config import token
+
+if token == "" or token is None:
+    raise ValueError("No token provided. Please set the token in config.py")
+else:
+    print("Token found.")
+if owner == "" or owner is None:
+    raise ValueError("No owner ID provided. Please set the owner ID in config.py")
+else:
+    print("Owner ID found.")
+if greet == "" or greet is None:
+    raise ValueError("No greet channel ID provided. Please set the greet channel ID in config.py")
+else:
+    print("Greet channel ID found.")
 
 bot = discord.Bot(
     intents=intents, 
@@ -19,7 +35,7 @@ bot = discord.Bot(
 
 @bot.event
 async def on_ready():
-    print(f"{bot.user} is Online and Connected to Discord!")
+    print(f"{bot.user} is Online and Connected to Discord")
 
 if __name__ == "__main__":
     for filename in os.listdir("cogs"):
