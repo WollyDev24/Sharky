1e41038ebd752506297be5aadb520e207863de95
WollyDev <wollywoll8844@gmail.com>
2025-10-03 17:30:45 +0200

    Update greet.py

--- CustomBot/cogs/greet.py
+++ CustomBot/cogs/greet.py
@@ -1,7 +1,9 @@
 import discord
 from discord.ext import commands
 from discord.commands import slash_command
+import os
 
+from config import greet
 
 class Greet(commands.Cog):
     def __init__(self, bot):
@@ -9,17 +11,17 @@ class Greet(commands.Cog):
 
     @slash_command()
     async def greet(self, ctx):
-        await ctx.respond(f"Hey {ctx.author.mention}")
+        await ctx.respond(f"Hey {ctx.author.mention}!")
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
         embed = discord.Embed(
             title="Welcome!",
-            description=f"Welcome to the server, {member.mention}!",
+            description=f"Welcome to the server, {member.mention}, Be sure to check the <#1400528778509422633>, look for giveaways in <#1400527952290123828>", # replace the channel ids with your own
             color=discord.Color.blue()
         )
     
-        channel = await self.bot.fetch_channel(1400531849008386170) # Replace with our own channel id
+        channel = await self.bot.fetch_channel(greet)
         await channel.send(embed=embed)
 
 def setup(bot):
