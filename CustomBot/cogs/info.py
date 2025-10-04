d7cbcb91e23de5532aa5f9294fdc29ac2999f218
WollyDev <wollywoll8844@gmail.com>
2025-10-05 01:41:19 +0200

    Update info.py

--- CustomBot/cogs/info.py
+++ CustomBot/cogs/info.py
@@ -15,7 +15,63 @@ class Info(commands.Cog):
             color=discord.Color.green()
         )
         embed.add_field(
+            name="Website",import discord
+from discord.ext import commands
+from discord.commands import slash_command
+
+
+class Info(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @slash_command(description="Information and Changelog")
+    async def info(self, ctx):
+        embed = discord.Embed(
+            title="Bot Info and Changelog",
+            description="",
+            color=discord.Color.green()
+        )
+        embed.add_field(
             name="Website",
+            value="[Sharky Website](https://wollydev24.github.io/sharky)", 
+            inline=False
+        )
+        embed.add_field(
+            name="Version", 
+            value="V1.5.1", 
+            inline=True
+            )
+        embed.add_field(
+            name="Relase Type", 
+            value="Stable", 
+            inline=True
+            )
+        embed.add_field(
+            name="Developer", 
+            value="[WollyDev24](https://github.com/WollyDev24)", 
+            inline=False
+            )
+        embed.add_field(
+            name="Github", 
+            value="[Github Repo](https://github.com/WollyDev24/Sharky/)", 
+            inline=False
+            )
+        embed.add_field(
+            name="Newest Update", 
+            value=" - Added cooldown to ``/ping``\n - Updated how infos look at startup in the termianl\n - ``/greet`` is now ``/hello``\n - Added ``/help`` command", 
+            inline=False
+            )
+        embed.add_field(
+            name="Remove restrictions", 
+            value="if you want to remove the restrictions (e.g. /activity can only be executed by the bot owner) build the bot yourself from [GitHub](https://wolldev24.github.io/sharky)", 
+            inline=False
+            )
+
+
+        await ctx.respond(embed=embed)
+
+def setup(bot):
+    bot.add_cog(Info(bot))
             value="[Sharky Website](https://wollydev24.github.io/sharky.html)", 
             inline=False
         )
