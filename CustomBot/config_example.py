6343813c00bd65efa338c141751258fc136ef27e
WollyDev24 <wollywoll8844@gmail.com>
2025-10-13 16:39:02 +0200

    Updated config_example.py

--- CustomBot/config_example.py
+++ CustomBot/config_example.py
@@ -1,3 +1,4 @@
+# Renamr this file to config.py
 token= "123456789010.ABCDEFGHIJK.KLMNOPQ.RSTUVWXYZ" # Replace this with your own token
 server= "1234456789" # Replace with your server id
 greet= "123456789" # replace with your greet channel id
